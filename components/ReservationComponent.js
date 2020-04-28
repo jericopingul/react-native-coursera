17fc56a368b8606de5e9029abf1732319d96f0b6
Jerico Pingul <jerico.pingul@gmail.com>
2020-04-28 22:21:23 +0100

    local notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -11,6 +11,8 @@ import {
 } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
   constructor(props) {
@@ -34,7 +36,7 @@ class Reservation extends Component {
 
   handleReservation() {
     console.log(JSON.stringify(this.state));
-    this.toggleModal();
+    this.showAlert();
   }
 
   resetForm() {
@@ -42,6 +44,37 @@ class Reservation extends Component {
       guests: 1,
       smoking: false,
       date: '',
+    });
+  }
+
+  async obtainNotificationPermission() {
+    let permission = await Permissions.getAsync(
+      Permissions.USER_FACING_NOTIFICATIONS
+    );
+    if (permission.status !== 'granted') {
+      permission = await Permissions.askAsync(
+        Permissions.USER_FACING_NOTIFICATIONS
+      );
+      if (permission.status !== 'granted') {
+        Alert.alert('Permission not granted to show notifications');
+      }
+    }
+    return permission;
+  }
+
+  async presentLocalNotification(date) {
+    await this.obtainNotificationPermission();
+    Notifications.presentLocalNotificationAsync({
+      title: 'Your Reservation',
+      body: 'Reservation for ' + date + ' requested',
+      ios: {
+        sound: true,
+      },
+      android: {
+        sound: true,
+        vibrate: true,
+        color: '#512DA8',
+      },
     });
   }
 
@@ -63,7 +96,10 @@ class Reservation extends Component {
         },
         {
           text: 'OK',
-          onPress: () => this.resetForm(),
+          onPress: () => {
+            this.presentLocalNotification(this.state.date);
+            this.resetForm();
+          },
         },
       ],
       { cancellable: false }
@@ -130,7 +166,7 @@ class Reservation extends Component {
           <Button
             title='Reserve'
             color='#512DA8'
-            onPress={() => this.showAlert()}
+            onPress={() => this.handleReservation()}
             accessibilityLabel='Learn more about this purple button'
           />
         </View>
